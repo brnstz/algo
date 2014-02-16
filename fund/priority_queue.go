79de975f9983bca978562717897fa723912353f2
Brian Seitz <brnstz@gmail.com>
2014-02-16 18:48:30 -0500

    - adding untested priority queue code

---
+++ fund/priority_queue.go
@@ -0,0 +1,201 @@
+package fund
+
+import (
+	"errors"
+)
+
+// A priority queue that can hold any set of items that implements
+// the PQItem interface
+type PriorityQueue struct {
+	// Our internal store of data. To make math earsier, we start
+	// storing data in data[1]. data[0] is unused.
+	data []PQItem
+
+	// The current number of actual items
+	n int
+
+	// The max number of items we can store
+	maxN int
+
+	// Given a PQItem, its index in our data array
+	revMap map[PQItem]int
+}
+
+// Implement this interface to use this type on a PriorityQueue
+type PQItem interface {
+	// Compare the receiver of the method call to item passed in args. The
+	// implementing function should use type assertion to convert the passed
+	// in item. Return true if receiver should be considered a lower
+	// priority than the argument, false otherwise.
+	PQLess(PQItem) bool
+}
+
+var NotFound = errors.New("item not found in priority queue")
+
+// Create a new priority queue with max size of maxN
+func NewPriorityQueue(maxN int) *PriorityQueue {
+	return &PriorityQueue{
+		data:   make([]PQItem, maxN+1),
+		maxN:   maxN,
+		revMap: make(map[PQItem]int),
+	}
+}
+
+// What is the index value of this item?
+func (pq *PriorityQueue) IndexOf(key PQItem) int {
+	return pq.revMap[key]
+}
+
+// Do we contain this item?
+func (pq *PriorityQueue) Contains(key PQItem) bool {
+	_, ok := pq.revMap[key]
+
+	return ok
+}
+
+// We have changed the value as used by PQLess() for this key. Sink and
+// swim above/below to be sure value in correct place.
+func (pq *PriorityQueue) IndicateChange(i int) {
+	pq.swim(i)
+	pq.sink(i)
+}
+
+// Insert a new value into our priority queue
+func (pq *PriorityQueue) Insert(key PQItem) {
+	// Increment the total number of items in the pq
+	pq.n++
+
+	// Start by storing the item at the end of our data array
+	pq.revMap[key] = pq.n
+	pq.data[pq.n] = key
+
+	// Restore heap order by swimming up to place it in the correct location
+	pq.swim(pq.n)
+}
+
+// Delete this item from the priority queue. Returns NotFound if item
+// is not currently in the queue.
+/*
+FIXME
+func (pq *PriorityQueue) Delete(key PQItem) error {
+	i := pq.IndexOf(key)
+
+
+}
+*/
+
+// Delete the highest priority item in our queue
+func (pq *PriorityQueue) DelMax() PQItem {
+	// The max item is index 1, get it
+	max := pq.data[1]
+
+	// Put the n-indexed item on top
+	pq.swap(1, pq.n)
+
+	// Remove from reverse map
+	delete(pq.revMap, pq.data[pq.n])
+
+	// Delete the old swapped max
+	pq.data[pq.n] = nil
+
+	// Our queue is one item shorter now
+	pq.n--
+
+	// Restore heap order
+	pq.sink(1)
+
+	return max
+}
+
+// Starting from k, "swim" higher priority values up toward index 1. This
+// restores heap order from k back to index 1 when a new value is placed at
+// k.
+func (pq *PriorityQueue) swim(k int) {
+	// While we are not at the top index, and there are still values
+	// out of heap order
+	for k > 1 && pq.data[k/2].PQLess(pq.data[k]) {
+
+		// Swap k with its left child
+		pq.swap(k/2, k)
+
+		// Go up priority tree one level
+		k = k / 2
+	}
+}
+
+// Starting with k, "sink" lower priority queue values down the tree. This
+// restores heap down from k down to the end of the queue when a new
+// value is inserted at k.
+func (pq *PriorityQueue) sink(k int) {
+	// While we have a left child of k within the size of our data
+	for 2*k <= pq.n {
+
+		// Find the indexes of the left and right children of k
+		leftChild := 2 * k
+		rightChild := 2*k + 1
+
+		// By default, assume left child is biggest, but if
+		// rightChild isn't greater than n, it could also
+		// be it.
+		j := leftChild
+
+		if rightChild <= pq.n && pq.data[leftChild].PQLess(pq.data[rightChild]) {
+			j = rightChild
+		}
+
+		// j has the largest child, now check against our parent
+		if pq.data[k].PQLess(pq.data[j]) {
+
+			// If the parent is less, we should swap with j
+			pq.swap(k, j)
+		}
+
+		// Set k for the next round down the tree
+		k = j
+	}
+}
+
+// Swap values at index i and j
+func (pq *PriorityQueue) swap(i, j int) {
+
+	// Swap the reverse map
+	pq.revMap[pq.data[i]], pq.revMap[pq.data[j]] = pq.revMap[pq.data[j]], pq.revMap[pq.data[i]]
+
+	// Swap the actual data
+	pq.data[i], pq.data[j] = pq.data[j], pq.data[i]
+
+}
+
+// Is our priority queue empty?
+func (pq *PriorityQueue) IsEmpty() bool {
+	return pq.n < 1
+}
+
+// What is the maximum size we can hold in our priority queue?
+func (pq *PriorityQueue) MaxSize() int {
+	return pq.maxN
+}
+
+/*
+func (pq *PQIndex) N() int {
+	return pq.n
+}
+*/
+
+/*
+FIXME this is not correct, and it should use i int for indexes, otherwise
+the map comparison will not work.
+func (pq *PQIndex) Delete(key PQItem) {
+	i := pq.IndexOf(key)
+
+	// Swap our deleted index with the last value
+	pq.n--
+
+	pq.swap(i, pq.n)
+
+	pq[pq.n] = nil
+
+	pq.swim(i)
+	pq.sink(i)
+}
+*/
