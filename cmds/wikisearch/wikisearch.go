3f12ce7d1b3addd12ebf7e26d5503e4bbdd46c2e
Brian Seitz <brnstz@gmail.com>
2017-09-20 00:21:41 -0400

    adding broken code

--- cmds/wikisearch/wikisearch.go
+++ cmds/wikisearch/wikisearch.go
@@ -2,23 +2,47 @@ package main
 
 import (
 	"bufio"
+	"compress/bzip2"
 	"encoding/json"
-	"io"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 
 	"github.com/brnstz/algo"
 )
 
-const MAX_COMPLETIONS = 100
-const WIKI_INDEX_URL = "https://dumps.wikimedia.org/enwiki/20170820/enwiki-20170820-pages-articles-multistream-index.txt.bz2"
-const LOAD_LOG_INTERVAL = 1000000
+const (
+	MAX_COMPLETIONS   = 100
+	LOAD_LOG_INTERVAL = 1000000
+	DUMP_DATE         = "20178020"
+	WIKI_INDEX_URL    = "http://dumps.wikimedia.your.org/%vwiki/%v/%vwiki-%v-pages-articles-multistream-index.txt.bz2"
+	WIKIS             = "en|ceb|sv|de|nl|fr|ru|it|es|war|pl|vi|ja|pt|zh|uk|fa|ca|ar|no|sh|fi|hu|id|ko"
+	DOWNLOAD_WORKERS  = 5
+)
 
 type wordResponse struct {
 	Exists      bool     `json:"exists"`
 	Completions []string `json:"completions"`
+}
+
+func download(url string, t *algo.Trie) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println("can't download %v: %v", url, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	reader, err := bzip2.NewReader(resp.Body)
+	if err != nil {
+		log.Println("can't read bzip2 data in %v: %v", url, err)
+		return
+	}
+
+	s := bufio.NewScanner(reader)
+	for s.Scan() {
+		t.Add(s.Text())
+	}
+
 }
 
 func getWord(t *algo.Trie, w http.ResponseWriter, r *http.Request) {
@@ -43,38 +67,18 @@ func getWord(t *algo.Trie, w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
-	i := 0
-	r := bufio.NewReader(os.Stdin)
 	trie := algo.NewTrie(0)
-
-	for ; ; i++ {
-		text, err := r.ReadString('\n')
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			log.Fatal(err)
-		}
-		text = strings.TrimSpace(text)
-
-		strings := strings.SplitN(text, ":", 3)
-
-		title := strings[2]
-
-		trie.Add(title)
-
-		if i > 0 && i%LOAD_LOG_INTERVAL == 0 {
-			log.Printf("loaded %v lines", i)
-		}
-
-	}
-	log.Printf("loading complete, %v lines\n", i)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/word", func(w http.ResponseWriter, r *http.Request) {
 		getWord(trie, w, r)
 	})
 	mux.Handle("/", http.FileServer(http.Dir("static")))
+
+	for i := 0; i < DOWNLOAD_WORKERS; i++ {
+		go download()
+
+	}
 
 	log.Fatal(http.ListenAndServe(":53172", mux))
 }
