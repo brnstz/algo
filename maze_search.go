80a54228531d319df87ad9c7ee47d028bd92a2b7
Brian Seitz <brnstz@gmail.com>
2018-06-08 09:09:41 -0400

    adding broken code

---
+++ maze_search.go
@@ -0,0 +1,26 @@
+package algo
+
+type mazeVertex struct {
+	x, y  int
+	edges []mazeEdge
+}
+
+type mazeEdge struct {
+	from, to *mazeVertex
+}
+
+func constructMaze(node *mazeVertex, maze [][]bool) {
+
+}
+
+// MazeSearch returns the path required to traverse a maze starting at
+// (startX, startY) and ending at (endX, endY)
+func MazeSearch(maze [][]bool, startX, startY, endX, endY int) [][]int {
+
+	root := &mazeVertex{
+		x: startX,
+		y: startY,
+	}
+
+	for 
+}
