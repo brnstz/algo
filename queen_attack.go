16d2f2d4935a55198db298321bd037be4e066d32
Brian Seitz <brnstz@gmail.com>
2018-06-24 13:44:09 -0400

    adding queen attack fixes

--- queen_attack.go
+++ queen_attack.go
@@ -1,5 +1,7 @@
 package algo
 
+import "fmt"
+
 // https://www.hackerrank.com/challenges/queens-attack-2/problem
 
 const (
@@ -15,7 +17,7 @@ func validMove(x, y int, grid [][]int) bool {
 		grid[x][y] == empty
 }
 
-// QueenAttack FIXME
+// QueenAttack returns the number of squares the queen at x, y can attack.
 func QueenAttack(n, numObstacles, x, y int, obstacles [][]int) int {
 	var (
 		squares   int
@@ -54,6 +56,58 @@ func QueenAttack(n, numObstacles, x, y int, obstacles [][]int) int {
 			ay += attack[1]
 
 			if validMove(ax, ay, grid) {
+				squares++
+			} else {
+				break
+			}
+		}
+	}
+
+	return squares
+}
+
+func qkey(x, y int) string {
+	return fmt.Sprintf("(%v,%v)", x, y)
+}
+
+func validMoveNoGrid(x, y, n int, omap map[string]bool) bool {
+	return x >= 0 && y >= 0 &&
+		x < n && y < n &&
+		!omap[qkey(x, y)]
+}
+
+// QueenAttackNoGrid returns the number of squares the queen at x, y can attack
+// without creating the entire grid.
+func QueenAttackNoGrid(n, numObstacles, x, y int, obstacles [][]int) int {
+	var (
+		squares int
+		ax, ay  int
+	)
+
+	omap := map[string]bool{}
+
+	// Set position of obstacles
+	for _, v := range obstacles {
+		omap[qkey(v[0]-1, v[1]-1)] = true
+	}
+
+	attackDirs := [][]int{
+		{0, 1}, {0, -1},
+		{1, 0}, {-1, 0},
+		{1, 1}, {-1, -1},
+		{-1, 1}, {1, -1},
+	}
+
+	squares = 0
+	for _, attack := range attackDirs {
+		ax = x - 1
+		ay = y - 1
+
+		for true {
+			ax += attack[0]
+			ay += attack[1]
+
+			if validMoveNoGrid(ax, ay, n, omap) {
 				squares++
 			} else {
 				break
