3bb1b2c68defce8227e0b09317f4fbecd6f6c811
Brian Seitz <brnstz@gmail.com>
2018-06-24 11:54:30 -0400

    finish iterative solution

--- non_divisible_subset.go
+++ non_divisible_subset.go
@@ -1,6 +1,7 @@
 package algo
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -85,5 +86,56 @@ func mnds(k int, s []int, cache map[string]int) int {
 // the sum of any two items is not divisible by k
 func MaxNonDivisibleSubset(k int, s []int) int {
 	cache := map[string]int{}
-	return mnds(k, s, cache)
+	result := mnds(k, s, cache)
+	fmt.Printf("result: %v, cache: %v", result, cache)
+
+	return result
+}
+
+// MaxNonDivisibleSubsetIterative returns the size of the maximal subset of s
+// such that the sum of any two items is not divisible by k by pairing lists of
+// remainders iteratively.
+func MaxNonDivisibleSubsetIterative(k int, s []int) int {
+	var remainder int
+
+	// Remainders is a mapping of the remainder of dividing a value in s by k,
+	// mapped to list of values in s that have that remainder
+	remainders := make([][]int, k+1)
+
+	// Create the mapping as described above
+	for _, v := range s {
+		remainder = v % k
+		remainders[remainder] = append(remainders[remainder], v)
+	}
+
+	// Each remainder has a corresponding entry in the list: the value
+	// that it needs to add up to k. Iterate through all of them and
+	// choose the bigger corresponding entry each time.
+	optimalSubset := []int{}
+	for i := 1; i <= k/2; i++ {
+
+		// If k is even, we want to skip the entry that is k/2.  Adding > 1
+		// value from this will result in a divisible pair. Instead, just add 1 from
+		// this list if it exists.
+		if i == k-i {
+			if len(remainders[i]) > 0 {
+				optimalSubset = append(optimalSubset, remainders[i][0])
+			}
+			continue
+		}
+
+		// Choose which of the remainder pair to include in the optimal subset
+		if len(remainders[i]) > len(remainders[k-i]) {
+			optimalSubset = append(optimalSubset, remainders[i]...)
+		} else {
+			optimalSubset = append(optimalSubset, remainders[k-i]...)
+		}
+	}
+
+	// We can also include exactly one entry that is evenly divisble
+	if len(remainders[0]) > 0 {
+		optimalSubset = append(optimalSubset, remainders[0][0])
+	}
+
+	return len(optimalSubset)
 }
--- non_divisible_subset_test.go
+++ non_divisible_subset_test.go
@@ -18,3 +18,18 @@ func TestNonDivisibleSubset2(t *testing.T) {
 	x := MaxNonDivisibleSubset(1, []int{1, 2, 3, 4, 5})
 	t.Fatal(x)
 }
+
+func TestNonDivisibleSubset3(t *testing.T) {
+	x := MaxNonDivisibleSubsetIterative(3, []int{1, 7, 2, 4})
+	t.Fatal(x)
+}
+
+func TestNonDivisibleSubset4(t *testing.T) {
+	x := MaxNonDivisibleSubsetIterative(5, []int{770528134, 663501748, 384261537, 800309024, 103668401, 538539662, 385488901, 101262949, 557792122, 46058493})
+	t.Fatal(x)
+}
+
+func TestNonDivisibleSubset5(t *testing.T) {
+	x := MaxNonDivisibleSubsetIterative(11, []int{582740017, 954896345, 590538156, 298333230, 859747706, 155195851, 331503493, 799588305, 164222042, 563356693, 80522822, 432354938, 652248359})
+	t.Fatal(x)
+}
