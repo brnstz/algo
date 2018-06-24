ac199e312979984b7f6928582817294f151a6e1d
Brian Seitz <brnstz@gmail.com>
2018-06-24 10:04:17 -0400

    cached version

--- non_divisible_subset.go
+++ non_divisible_subset.go
@@ -1,5 +1,10 @@
 package algo
 
+import (
+	"strconv"
+	"strings"
+)
+
 // https://www.hackerrank.com/challenges/non-divisible-subset/problem
 
 func isNonDivisibleSubset(k int, s []int) bool {
@@ -20,14 +25,38 @@ func isNonDivisibleSubset(k int, s []int) bool {
 	return true
 }
 
-// MaxNonDivisibleSubset returns the size of the maximal subset of s such that
-// the sum of any two items is not divisible by k
-func MaxNonDivisibleSubset(k int, s []int) int {
+// mndsKey returns a key for a cached version of a subset solution, assuming
+// that repeated attempts for s are always in the same order
+func mndsKey(s []int) string {
+	k := make([]string, len(s))
+	for i, v := range s {
+		k[i] = strconv.Itoa(v)
+	}
+
+	return strings.Join(k, "|")
+}
+
+func mnds(k int, s []int, cache map[string]int) int {
+	var (
+		key    string
+		value  int
+		exists bool
+	)
+
+	// Check the cache first
+	key = mndsKey(s)
+	value, exists = cache[key]
+	if exists {
+		return value
+	}
+
 	if len(s) < 2 {
+		cache[key] = 0
 		return 0
 	}
 
 	if isNonDivisibleSubset(k, s) {
+		cache[key] = len(s)
 		return len(s)
 	}
 
@@ -41,12 +70,20 @@ func MaxNonDivisibleSubset(k int, s []int) int {
 		newS = append(newS, s[:i]...)
 		newS = append(newS, s[i+1:]...)
 
-		thisSubset := MaxNonDivisibleSubset(k, newS)
+		thisSubset := mnds(k, newS, cache)
 
 		if thisSubset > maxSubset {
 			maxSubset = thisSubset
 		}
 	}
 
+	cache[key] = maxSubset
 	return maxSubset
+}
+
+// MaxNonDivisibleSubset returns the size of the maximal subset of s such that
+// the sum of any two items is not divisible by k
+func MaxNonDivisibleSubset(k int, s []int) int {
+	cache := map[string]int{}
+	return mnds(k, s, cache)
 }
