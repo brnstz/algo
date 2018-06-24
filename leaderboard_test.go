59175218bf0a477086dd805008df70471638b058
Brian Seitz <brnstz@gmail.com>
2018-06-23 21:43:02 -0400

    testin

--- leaderboard_test.go
+++ leaderboard_test.go
@@ -3,5 +3,5 @@ package algo
 import "testing"
 
 func TestLeaderboard(t *testing.T) {
-	t.Fatal(Leaderboard([]int{100, 90, 90, 90, 80, 80, 80, 79}, []int{70, 75, 80, 90, 100, 110, 99}))
+	t.Fatal(Leaderboard([]int{100, 90, 90, 90, 80, 80, 80, 79}, []int{66, 70, 75, 80, 90, 100, 110, 99}))
 }
