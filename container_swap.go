cdb8f8441170624e8b79b13431265835445a5f57
Brian Seitz <brnstz@gmail.com>
2018-06-27 04:20:27 -0400

    final solution

--- container_swap.go
+++ container_swap.go
@@ -1,119 +1,49 @@
 package algo
 
-import "fmt"
-
 // https://www.hackerrank.com/challenges/organizing-containers-of-balls/problem
 
-func swappable(container [][]int, n int, assigned int, totals []int, ballAssignment []int, containerUsed map[int]bool) bool {
-	// totals[i] = the total number of balls of type i
-	// bA[i] = ball i is assigned to container bA[i]
-	// cA[i] = container i is assigned to ball cA[i]
-
-	fmt.Printf("%v %v %v %v %v %v\n\n",
-		container, n, assigned, totals, ballAssignment, containerUsed,
-	)
-
-	var (
-		deficit, i, j int
-	)
-
-	if assigned == n {
-		for i = 0; i < n; i++ {
-			deficit += totals[i] - container[ballAssignment[i]][i]
-		}
-
-		fmt.Printf("what is the deficit? %v\n", deficit)
-
-		return deficit%2 == 0
-	}
-
-	// Assign the next ball
-	assigned++
-
-	// Assign the ball to all possible containers
-	for j = 0; j < n; j++ {
-		if containerUsed[j] {
-			continue
-		}
-
-		newCU := map[int]bool{}
-		for k, v := range containerUsed {
-			newCU[k] = v
-		}
-		newCU[j] = true
-
-		newBA := make([]int, len(ballAssignment)+1)
-		copy(newBA, ballAssignment)
-		fmt.Printf("%v %v %v\n", newBA, ballAssignment, assigned)
-		newBA[assigned-1] = j
-
-		if swappable(container, n, assigned, totals, newBA, newCU) {
-			return true
-		}
-
-	}
-
-	/*
-		for j = 0; j < n; j++ {
-			if bA[i] == -1 && cA[j] == -1 {
-				newBA := make([]int, n)
-				newCA := make([]int, n)
-				copy(newBA, bA)
-				copy(newCA, cA)
-
-				newBA[i] = j
-				newBA[j] = i
-
-				if swappable(container, n, totals, newBA, newCA) {
-					return true
-				}
-			}
-		}
-	*/
-
-	return false
-}
-
-// ContainerSwap FIXME
+// ContainerSwap determines if, given container[i][j] all balls of type j can
+// be put into a single container i by making a series of swaps of balls
+// between containers.
 func ContainerSwap(container [][]int) bool {
 	var (
 		i, j, n int
 
-		totals []int
+		containerTotals []int
+		ballTotals      []int
+
+		counts map[int]bool
 	)
 
 	// Assume square matrix
 	n = len(container)
 
-	totals = make([]int, n)
+	// containerTotals[i] is the number of all balls in container i
+	containerTotals = make([]int, n)
+
+	// ballTotals[j] is the number of ball type j in all container
+	ballTotals = make([]int, n)
+
+	// counts is a mapping of total values that should exist in both
+	// containerTotals and ballTotals
+	counts = map[int]bool{}
 
 	for i = 0; i < n; i++ {
 		for j = 0; j < n; j++ {
-			totals[j] += container[i][j]
+			containerTotals[i] += container[i][j]
+			ballTotals[j] += container[i][j]
 		}
 	}
-	containerUsed := map[int]bool{}
 
-	return swappable(container, n, 0, totals, nil, containerUsed)
+	for i = 0; i < n; i++ {
+		counts[ballTotals[i]] = true
+	}
+
+	for i = 0; i < n; i++ {
+		if !counts[containerTotals[i]] {
+			return false
+		}
+	}
+
+	return true
 }
-
-// - each container contains only balls of the same type
-// - no two balls of the same type are located in different
-//   containers
-
-// for each row:
-//   - one column is >= 0
-//   - all other columns are == 0
-//   - no two rows have same column >= 0
-
-// brute force solution
-//	- find all possible combinations of rows and columns
-//		(e.g., assign a column to be >= to a row)
-//  - recursively perform all moves leading to that row
-//		matching the conditions
-
-// more optimized solution
-// - don't actually solve the problem, just figure out if it's
-//	 possible
-//
-// - what are the cases where it wouldn't be possible?
