f39edb52d5079a2b20d4df9752bfbbe76da1ea6c
Brian Seitz <brnstz@gmail.com>
2018-06-30 18:57:15 -0400

    fixing test

--- puzzles/cut_the_tree_test.go
+++ puzzles/cut_the_tree_test.go
@@ -7,7 +7,9 @@ func TestCutTree1(t *testing.T) {
 		[]int{100, 200, 100, 500, 100, 600},
 		[][]int{{1, 2}, {2, 3}, {2, 5}, {4, 5}, {5, 6}})
 
-	t.Fatal(x)
+	if x != 400 {
+		t.Fatalf("expected min diff %v but got %v", 400, x)
+	}
 }
 
 func TestCutTree2(t *testing.T) {
@@ -15,5 +17,7 @@ func TestCutTree2(t *testing.T) {
 		[]int{1, 2, 3, 4, 5, 6},
 		[][]int{{1, 2}, {1, 3}, {2, 6}, {3, 4}, {3, 5}})
 
-	t.Fatal(x)
+	if x != 3 {
+		t.Fatalf("expected min diff %v but got %v", 3, x)
+	}
 }
